bc333f46b6dab017bdce0fb1beafbae927a721f5
filipe-lima-vaz <filipe.lima@dcx.ufpb.br>
2024-05-30 21:04:19 -0300

    language fixes

--- server/src/app/repositories/TodoRepository.js
+++ server/src/app/repositories/TodoRepository.js
@@ -4,28 +4,28 @@ class TodoRepository {
     async create(todo) {
         const { title, description, status, creation_date } = todo;
         const sql = "INSERT INTO todos (title, description, status, creation_date) VALUES ($1, $2, $3, COALESCE($4, CURRENT_TIMESTAMP)) RETURNING *";
-        return await connectionQuery(sql, [title, description, status, creation_date], "Error creating todo");
+        return await connectionQuery(sql, [title, description, status, creation_date], "Erro ao criar tarefa");
     }
 
     async findAll() {
         const sql = "SELECT * FROM todos";
-        return await connectionQuery(sql, [], "Error retrieving all todos");
+        return await connectionQuery(sql, [], "Erro ao retornar todas as tarefas");
     }
 
     async findById(id) {
         const sql = "SELECT * FROM todos WHERE id = $1";
-        return await connectionQuery(sql, [id], "Error retrieving todo by id");
+        return await connectionQuery(sql, [id], "Erro ao retornar a tarefa por id");
     }
 
     async update(id, todo) {
         const { title, description, status, creation_date } = todo;
         const sql = "UPDATE todos SET title = $1, description = $2, status = $3, creation_date = $4 WHERE id = $5 RETURNING *";
-        return await connectionQuery(sql, [title, description, status, creation_date, id], "Error updating todo");
+        return await connectionQuery(sql, [title, description, status, creation_date, id], "Erro ao atualizar tarefa");
     }
 
     async delete(id) {
         const sql = "DELETE FROM todos WHERE id = $1 RETURNING *";
-        return await connectionQuery(sql, [id], "Error deleting todo");
+        return await connectionQuery(sql, [id], "Erro ao apagar tarefa");
     }
 }
 
--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server working on http://localhost:${port}`);
+    console.log(`Servidor rodando no endereço http://localhost:${port}`);
 });
 
 
